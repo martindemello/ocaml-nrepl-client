66717982e052256edb365d984777a1e2166904ff
Martin DeMello <martindemello@gmail.com>
2011-05-15 23:47:39 +0530

    add some internal /commands

--- makefile
+++ makefile
@@ -7,7 +7,7 @@ SOURCES = \
 
 GODI = /home/martin/opt/godi/lib/ocaml
 #LIBS = unix bigarray str pcre run_mikmatch_pcre
-PACKS = unix bigarray str mikmatch_pcre pcre batteries aurochs_lib
+PACKS = unix bigarray str mikmatch_pcre pcre batteries
 INCDIRS = $(GODI)/pkg-lib/batteries $(GODI)/std-lib/camlp5 $(GODI)/pkg-lib/pcre $(GODI)/pkg-lib/mikmatch_pcre
 CREATE_LIB = yes
 PRE_TARGETS = ledit/pa_local.cmo ledit/pa_def.cmo
--- nrepl_client.ml
+++ nrepl_client.ml
@@ -1,6 +1,6 @@
 (*pp $PP *)
 (*************************************************************************
- * Requires: 
+ * Requires:
  *   str.cma
  *************************************************************************)
 
@@ -12,6 +12,10 @@ type repl = {
   current_exp : string option;
 }
 
+type env = {
+  mutable repl: repl;
+}
+
 let initial_repl = {
   ns          = "user";
   debug       = false;
@@ -20,9 +24,34 @@ let initial_repl = {
 
 let prompt_of repl = String.concat "" [repl.ns; ">> "]
 
+(*************************************************************************
+ * internal commands
+ * ***********************************************************************)
+
+let display_help () =
+  printf "Type something!\n";
+  flush stdout
+
+let set_debug repl o =
+  let d = match o with
+  | "true"  -> true
+  | "on"    -> true
+  | "false" -> false
+  | "off"   -> false
+  | _       -> repl.debug
+  in
+  printf "debug = %s\n" (if d then "true" else "false");
+  flush stdout;
+  {repl with debug = d}
+
+let handle_cmd repl cmd =
+  match Str.bounded_split (Str.regexp " +") cmd 2 with
+  | ["/help"]  -> display_help (); repl
+  | ["/debug"; o] -> set_debug repl o
+  | _                -> repl
 
 (*************************************************************************
- * main() and friends
+ * repl
  * ***********************************************************************)
 
 let readline prompt =
@@ -40,16 +69,25 @@ let bad_command () =
   printf "Bad command\n";
   flush stdout
 
-let handle str =
-  printf "%s\n" str;
-  flush stdout
+let send_cmd repl str =
+  printf "--> %s\n" str;
+  flush stdout;
+  repl
+
+let handle repl str =
+  if String.length str == 0 then
+    repl
+  else if str.[0] == '/' then
+    handle_cmd repl str
+  else
+    send_cmd repl str
 
 let _ =
   try
-    let repl = initial_repl in
+    let r = {repl = initial_repl} in
     while true do
-      let str = readline (prompt_of repl) in
-      handle str;
+      let str = readline (prompt_of r.repl) in
+      r.repl <- handle r.repl str;
     done;
     flush stdout;
   with End_of_file -> print_newline ();
