268ca300ff3e8433e381a40c04cdafcc27c83e29
Martin DeMello <martindemello@gmail.com>
2011-05-25 21:41:15 +0530

    fix string utils

--- src/util.ml
+++ src/util.ml
@@ -5,7 +5,6 @@ open String
 
 (* utility functions *)
 
-
 let split x y = Str.split (Str.regexp x) y
 
 let lines x = split "\n" x
@@ -14,7 +13,12 @@ let unlines xs = concat "\n" xs
 
 let q str = sprintf "\"%s\"" str
 
-let uq str = strip ~chars:"\"" str 
+let uq str =
+  let l = String.length str - 1 in
+  if str.[0] == '"' && str.[l] == '"' then
+    String.sub str 1 (l - 1)
+  else
+    str
 
 let unsome default = function
   | None -> default
@@ -25,10 +29,10 @@ let notnone x = x != None
 let us x = unsome "" x
 
 let ends_with src pat =
-  if ((String.length src) >= (String.length pat)) then
-    (pat =
-     String.sub src
-       ((String.length src) - (String.length pat)) (String.length pat))
+  let l = String.length src in
+  let m = String.length pat in
+  if l >= m then
+    pat = String.sub src (l - m) m
   else
     false
 
@@ -45,19 +49,3 @@ let split path =
     if path = Filename.current_dir_name then []
     else (Filename.basename path) :: aux (Filename.dirname path)
   in List.rev (aux path)
-
-let strip str =
-    let len = String.length str
-    and is_space char = (char = ' ' || char = '\t') in
-    let start = ref 0
-    and stop = ref (len - 1)
-    and seen_non_space = ref false in
-    for ii = 0 to len - 1 do
-      if is_space str.[ii] then
-        (if not !seen_non_space then
-          start := ii + 1)
-      else
-        (seen_non_space := true;
-         stop := ii)
-    done;
-    String.sub str !start (!stop - !start +1)
